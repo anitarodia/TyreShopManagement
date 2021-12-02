c7def3b027c08bd28eb924e26fec824703490554
anitarodia <61413897+anitarodia@users.noreply.github.com>
2021-12-02 18:40:12 +0530

    A project aimed at Tire Retailers all over India
    The project includes Statistics, GST Calculation, Inventory Management and all the functionalities required for Tire Retailers.

---
+++ src/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Animesh
+ */
+public class MainClass {
+    //change your mysql database connection here
+    public String StrUrl="jdbc:mysql://localhost:3306/tyre_shop?zeroDateTimeBehavior=convertToNull";
+    public String StrUid="root";
+    public String StrPwd= "kvuc";
+    
+    public static String StrUser;
+}
+
